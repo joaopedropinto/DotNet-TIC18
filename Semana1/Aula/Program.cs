baa98a93d5d40572314e268722a8348a43175dba
joaopedropinto <jpcic07@gmail.com>
2023-11-08 12:31:34 -0300

    Inicio do material da aula

--- Semana1/Aula/Program.cs
+++ Semana1/Aula/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region tipos de dados
+
+int tipoInteiro;
+double tipoDouble;
+bool tipoBool;
+string tipoTexto;
+
+tipoInteiro = int.MaxValue;
+System.Console.WriteLine(tipoInteiro);
+
+#endregion
