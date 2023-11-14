8698335696db379037824b34d61c87acddd8af58
joaopedropinto <jpcic07@gmail.com>
2023-11-14 12:13:39 -0300

    Adding Date examples

--- Semana2/Aula/Program.cs
+++ Semana2/Aula/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-#region Foreach example
+﻿#region Foreach example
 Console.WriteLine("Foreach example");
 string[] colecao= { "Item1", "Item2", "Item3", "Item4" };
 
@@ -55,4 +53,49 @@ Console.WriteLine($"Hello, {name1.ToUpper()}");
 Console.WriteLine($"Hello, {name1.ToLower()}");
 Console.WriteLine($"Length of name: {name1.Length}");
 
+#endregion
+
+#region List example
+Console.WriteLine("List example");
+List<int> numbers = new List<int>();
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Remove(3);
+
+numbers.Add(20);
+numbers.Add(4);
+numbers.Sort();
+numbers.Remove(4);
+Console.WriteLine("List of integers:");
+foreach(int number in numbers){
+  Console.WriteLine(number);
+  
+}
+Console.WriteLine($"List has {numbers.Count} elements\n");
+
+List<List<string>> names2 = new List<List<string>>();
+names2.Add(new List<string>(){"João", "Pedro", "Pinto"}); 
+names2.Add(new List<string>(){"Samuel", "De", "Gois"});
+foreach(List<string> names3 in names2){
+
+  foreach(String name in names3){
+    Console.WriteLine(name);
+  }
+}
+#endregion 
+
+#region Date example
+  DateTime now = DateTime.Now;
+  Console.WriteLine($"Current date and time: {now}");
+  DateTime today = DateTime.Today;
+  Console.WriteLine($"Todays's date: {today}");
+  DateTime tomorrow = now.AddDays(1);
+  Console.WriteLine($"Tomorrow's date: {tomorrow}");
+  DateTime yesterday = now.AddDays(-1);
+  Console.WriteLine($"Yesterday's date: {yesterday}");
+  DateTime specificDate = new DateTime(2022, 02, 21);
+  Console.WriteLine($"Week Day: {specificDate.DayOfWeek}");
 #endregion
